791f92c88085c6487726bfc6aa3d380537053b8d
Zaya <zayazaw67@gmail.com>
2019-09-11 16:44:53 -0500

    created object with filler questions

---
+++ assets/javascript/app.js
@@ -0,0 +1,56 @@
+// setInterval()
+var trivia = [
+    {
+        question1: "insert multiple choice question",
+        answer1: {
+            a: "choice1",
+            b: "choice2",
+            c: "choice3",
+        },
+    correctAnswer: b
+    },
+    {
+        question1: "insert multiple choice question",
+        answer1: {
+            a: "choice1",
+            b: "choice2",
+            c: "choice3",
+        },
+    correctAnswer: a
+    },
+    {
+        question1: "insert multiple choice question",
+        answer1: {
+            a: "choice1",
+            b: "choice2",
+            c: "choice3",
+        },
+    correctAnswer: b
+    },
+    {
+        question1: "insert multiple choice question",
+        answer1: {
+            a: "choice1",
+            b: "choice2",
+            c: "choice3",
+        },
+    correctAnswer: c
+    }
+]
+var correct = 0;
+var incorrect = 0;
+timer = 180;
+
+function startgame(
+    correct = 0,
+    incorrect = 0,
+)
+
+
+// var questions = [
+//     { q: "The sky is blue.", a: "t" },
+//     { q: "There are 365 days in a year.", a: "t" },
+//     { q: "There are 42 ounces in a pound.", a: "f" },
+//     { q: "The Declaration of Independence was created in 1745.", a: "f" },
+//     { q: "Bananas are vegetables.", a: "f" }
+//   ];
